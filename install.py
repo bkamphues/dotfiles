9e258f462a367fc392c288d5dde3a5c89c4fb8db
Bo Kamphues <b.kamphues@outlook.com>
2021-02-03 15:22:44 +0100

    Added install.py and .gitignore

---
+++ install.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import subprocess
+from os.path import dirname
+from os.path import realpath
+from os.path import expanduser
+
+# variables
+HOME_DIRECTORY = expanduser("~")
+DOTFILES_DIRECTORY = dirname(realpath(__file__))
+
+# execute a command line command
+def execute(command):
+
+    # check if command is of type array
+    if not isinstance(command, list):
+        raise Exception("A type other than an array was given.")
+
+    # open a subprocess to execute the command and print any errors
+    process = subprocess.Popen(command, stderr=subprocess.PIPE, universal_newlines=True)
+    stdout, stderr = process.communicate()
+    if stdout:
+        print(stdout)
+    if stderr:
+        print(stderr)
+
+
+# create a symlink between two locations/files
+def symlink(file, link):
+
+    # open a subprocess to execute the command and print any errors
+    execute(["ln", "-vs", file, link])
+
+
+# create symlinks to dotfiles
+zshrc = f"{DOTFILES_DIRECTORY}/.zshrc"
+symlink(zshrc, HOME_DIRECTORY)
