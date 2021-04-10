b6cf5af2d4e901ce7d207b91223a9cc43014c05d
Bo Kamphues <b.kamphues@outlook.com>
2021-04-10 15:26:28 +0200

    Add initial installs

--- .config/yadm/bootstrap_ubuntu.py
+++ .config/yadm/bootstrap_ubuntu.py
@@ -4,4 +4,27 @@
 #################### By Bo Kamphues ########################
 ############################################################
 
+import subprocess
+
 print("Executing Ubuntu Bootstrap Python Script...")
+
+# library
+def __simpleRun(commands: list):
+    subprocess.run(commands)
+
+
+# update & upgrade apt-get
+__simpleRun(["sudo", "apt-get", "update"])
+__simpleRun(["sudo", "apt-get", "upgrade", "-y"])
+
+# install pip
+__simpleRun(["sudo", "apt-get", "install", "-y", "python3-pip"])
+
+# install global pip packages
+__simpleRun(["pip3", "install", "black"])
+__simpleRun(["pip3", "install", "termcolor"])
+
+from termcolor import colored, cprint
+
+text = colored("Hello world!", "red", attrs=["reverse", "blink"])
+print(text)
